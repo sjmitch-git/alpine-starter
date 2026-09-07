cdfa3815211155d67d30d19e17346aab75d2259c
sjmitch-git <sjmitch20@outlook.com>
2026-09-07 19:07:31 +0100

    Adds staticfiles dir

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Email

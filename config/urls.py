c7aa0b815d09a1f53580c3614bed1a6bbafbde25
sjmitch-git <sjmitch20@outlook.com>
2026-09-08 22:56:33 +0100

    Adds robots.txt

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("core.urls")),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
 
 if settings.DEBUG:

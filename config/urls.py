ae008daee16aa5c3459e5cbe2c6477195ea0210d
sjmitch-git <sjmitch20@outlook.com>
2026-09-09 04:28:40 +0100

    Adds sitemap.xml

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,10 @@ urlpatterns = [
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
+    path(
+        "sitemap.xml",
+        TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml"),
+    ),
 ]
 
 if settings.DEBUG:
